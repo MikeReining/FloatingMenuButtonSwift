e9ca53fae2c994b04b6cfbd3ccfc2edc23ad2143
Mike Reining <emailmike@gmail.com>
2015-02-18 13:15:14 -0800

    Added UIColor extension

---
+++ FloatingMenuButtonSwift/FloatingButton.swift
@@ -0,0 +1,35 @@
+//
+//  FloatingButton.swift
+//  FloatingMenuButtonSwift
+//
+//  Created by Michael Reining on 2/18/15.
+//  Copyright (c) 2015 Mike Reining. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class FloatingButton: UIButton {
+
+}
+
+
+extension UIColor {
+    class func flatWhiteColor() -> UIColor {
+        return UIColor(red: 0.1674, green: 0.1674, blue: 0.1674, alpha: 1.0)
+    }
+    
+    class func flatBlackColor() -> UIColor {
+        return UIColor(red: 0.1674, green: 0.1674, blue: 0.1674, alpha: 1.0)
+    }
+
+    class func flatBlueColor() -> UIColor {
+        return UIColor(red: 0.3132, green: 0.3974, blue: 0.6365, alpha: 1.0)
+    }
+
+    class func flatRedColor() -> UIColor {
+        return UIColor(red: 0.9115, green: 0.2994, blue: 0.2335, alpha: 1.0)
+    }
+    
+    
+}
