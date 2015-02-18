ae4cdc8bfb41a4e33d5af48248084d87bd3c7fa0
Mike Reining <emailmike@gmail.com>
2015-02-18 09:47:23 -0800

    Initial setup

---
+++ FloatingMenuButtonSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FloatingMenuButtonSwift
+//
+//  Created by Michael Reining on 2/18/15.
+//  Copyright (c) 2015 Mike Reining. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
