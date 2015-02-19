5fb8f26b7d7cd59b39d5a0deb393ebe6a1b3da7c
Mike Reining <emailmike@gmail.com>
2015-02-19 14:51:30 -0800

    Implemented button rotation

--- FloatingMenuController.swift
+++ FloatingMenuController.swift
@@ -45,20 +45,46 @@ class FloatingMenuController: UIViewController {
         return CGPoint(x: point.x, y: point.y - offset)
     }
     
-    func configureButtons() {
+    func positionButtons(visible: Bool) {
         let parentController = presentingViewController!
         let center = parentController.view.convertPoint(fromView.center, fromView: fromView.superview)
         
         closeButton.center = center
         
+        if visible {
+            closeButton.alpha = 1
+            closeButton.transform = CGAffineTransformMakeRotation(CGFloat(M_PI_2))
+
+            
+        } else {
+            closeButton.alpha = 0
+        }
+        
+        
         for (index, button) in enumerate(buttonArray) {
             var buttonOffset = buttonPadding * CGFloat (index + 1)
-            button.center = buttonOffsetPoint(center, offset: buttonOffset)
+            if visible {
+                button.center = buttonOffsetPoint(center, offset: buttonOffset)
+                buttonOffset += buttonPadding
+                button.transform = CGAffineTransformIdentity
+            } else {
+                button.center = center
+                button.transform = CGAffineTransformMakeRotation(CGFloat(M_PI_4))
+
+            }
+            
             button.addTarget(self, action: "menuButtonPressed:", forControlEvents: .TouchUpInside)
-            
-            buttonOffset += buttonPadding
-            
+
         }
+    }
+    
+    func animateButtons(toVisible: Bool) {
+        positionButtons(!toVisible)
+        
+        UIView.animateWithDuration(0.4, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0, options: .allZeros, animations: {
+            [self]
+            self.positionButtons(toVisible)
+            }, completion: nil)
     }
     
     override func viewDidLoad() {
@@ -77,7 +103,12 @@ class FloatingMenuController: UIViewController {
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        configureButtons()
+        animateButtons(true)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        animateButtons(false)
     }
     
     func closeMenu(sender: UIButton) {
