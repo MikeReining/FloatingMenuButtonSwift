e4cb0f76fa9e538044b30539cfef714bb00f88e1
Mike Reining <emailmike@gmail.com>
2015-02-19 11:11:07 -0800

    Refactored floating button

--- FloatingMenuButtonSwift/FloatingButton.swift
+++ FloatingMenuButtonSwift/FloatingButton.swift
@@ -10,44 +10,15 @@ import UIKit
 
 @IBDesignable
 class FloatingButton: UIButton {
- 
     
     func setup() {
-        self.tintColor = UIColor.flatWhiteColor()
-        self.backgroundColor = UIColor.flatBlueColor()
-        self.layer.cornerRadius = self.frame.width / 2
-        self.layer.masksToBounds = true
-        self.setBackgroundImage(UIColor.pixelImage(), forState: .Highlighted)
+        tintColor = UIColor.whiteColor()
+        if backgroundImageForState(.Normal) == nil {
+            setBackgroundImage(UIColor.flatBlueColor.pixelImage, forState: .Normal)
+        }
+        
+        layer.cornerRadius = frame.width/2
+        layer.masksToBounds = true
     }
-    
 }
 
-
-extension UIColor {
-    class func flatWhiteColor() -> UIColor {
-        return UIColor(red: 0.9274, green: 0.9436, blue: 0.95, alpha: 1.0)
-    }
-    
-    class func flatBlackColor() -> UIColor {
-        return UIColor(red: 0.1674, green: 0.1674, blue: 0.1674, alpha: 1.0)
-    }
-
-    class func flatBlueColor() -> UIColor {
-        return UIColor(red: 0.3132, green: 0.3974, blue: 0.6365, alpha: 1.0)
-    }
-
-    class func flatRedColor() -> UIColor {
-        return UIColor(red: 0.9115, green: 0.2994, blue: 0.2335, alpha: 1.0)
-    }
-    
-    class func pixelImage() -> UIImage {
-        UIGraphicsBeginImageContext(CGSizeMake(1, 1))
-        var context = UIGraphicsGetCurrentContext()
-        CGContextSetFillColorWithColor(context, self.blackColor().CGColor)
-        CGContextFillRect(context, CGRectMake(0, 0, 1, 1))
-        var pixelImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return pixelImage
-    }
-    
-}
